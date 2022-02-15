bc30c7a12857c253c9b8a69b1ab749608e4d760a
MasrurNeel <mikum861@gmail.com>
2022-02-15 11:14:10 +0600

    part4

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/test', function (){
-    return 'OK';
+    return 'changes this text';
 });
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
